5fd6a3513e5072e1cf590a675714e5dd8f05ef77
Priyamsinh Vaghela <W0851705@myscc.ca>
2024-10-10 08:34:30 -0400

    added a method that will return an observable in the serviced and added dependency injection and initiated the instance for the component using ngoninit

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {Books} from "../models/books";
 import {BookListItemComponent} from "../book-list-item/book-list-item.component";
 import {NgClass, NgFor} from "@angular/common";
+import {BookService} from "../services/book.service";
 
 @Component({
   selector: 'app-book-list',
@@ -16,4 +17,18 @@ import {NgClass, NgFor} from "@angular/common";
 })
 export class BookListComponent {
 
-}
+  displayedColumns:string[]= ['id', 'title', 'author', 'genre', 'availability'];
+  bookList: Books[] = [];
+  constructor (private bookService: BookService) {
+  }
+    ngOnInit()
+    {
+
+      this.bookService.getbooks().subscribe({
+        next: (data: Books[]) => this.bookList = data,
+        error: err => console.error("Error fetching Students", err),
+        complete: () => console.log("Student data fetch complete!")
+      })
+    }
+  }
+
--- src/app/services/book.service.ts
+++ src/app/services/book.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import {booklist} from "../models/mock-book.ts";
+import {Books} from "../models/books";
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookService {
-
+private books : Books[] = booklist;
   constructor() { }
+
+  getbooks() : Observable<Books[]>{
+    return of(booklist);
+  }
+
+
 }
