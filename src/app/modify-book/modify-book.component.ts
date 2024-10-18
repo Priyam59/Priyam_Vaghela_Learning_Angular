84e64a01ade676e5abe5193bf3896a9d34d94e4f
Priyamsinh Vaghela <W0851705@myscc.ca>
2024-10-18 14:57:01 -0400

    added a router And then added a list of routes in my main.ts while also adding two new component named modify-book and page not found and its routes in the main.ts

---
+++ src/app/modify-book/modify-book.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modify-book',
+  standalone: true,
+  imports: [],
+  templateUrl: './modify-book.component.html',
+  styleUrl: './modify-book.component.css'
+})
+export class ModifyBookComponent {
+
+}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,18 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import {bootstrapApplication} from '@angular/platform-browser';
+import {AppComponent} from './app/app.component';
+import {provideRouter, Routes} from '@angular/router';
+import {BookListComponent} from "./app/book-list/book-list.component";
+import {BookListItemComponent} from "./app/book-list-item/book-list-item.component";
+import {PageNotFoundComponent} from "./app/page-not-found/page-not-found.component";
+import {ModifyBookComponent} from "./app/modify-book/modify-book.component";
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+const routes: Routes = [
+  {path:'', redirectTo: '/books', pathMatch: 'full'},
+  { path: 'students', component: BookListComponent },
+  { path: 'students/:id', component: BookListItemComponent },
+  {path:'modify-student', component: ModifyBookComponent},
+  {path: '**', component:PageNotFoundComponent}
+];
+bootstrapApplication(AppComponent, {
+  providers: [provideRouter(routes)]
+}).catch((err) => console.error(err));
