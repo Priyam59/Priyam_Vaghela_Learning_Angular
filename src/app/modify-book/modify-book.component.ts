b0a736b0b964aad466afb10839f5bbe560631635
Priyamsinh Vaghela <W0851705@myscc.ca>
2024-11-20 18:13:13 -0500

    Created edit and delete buttons in html and methods in ts

--- src/app/modify-book/modify-book.component.ts
+++ src/app/modify-book/modify-book.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {BookService} from "../services/book.service";
+import {ActivatedRoute, Router} from "@angular/router";
+import {Books} from "../models/books";
 
 @Component({
   selector: 'app-modify-book',
@@ -7,6 +11,60 @@ import { Component } from '@angular/core';
   templateUrl: './modify-book.component.html',
   styleUrl: './modify-book.component.css'
 })
-export class ModifyBookComponent {
+export class ModifyBookComponent implements OnInit{
+  bookForm: FormGroup;
+  book: Books | undefined;
+  error: string | null = null;
 
+
+  constructor(
+    private fb: FormBuilder,
+    private route: ActivatedRoute,
+    private bookService: BookService,
+    private router: Router
+  ) {
+    this.bookForm = this.fb.group({
+      id: [bookService.generateNewId()],
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      department: [''],
+      isAdmin: [false]
+    });
+  }
+  ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    if (id) {
+      this.bookService.getBookById(id).subscribe( {
+        next: book => {
+          if (book) {
+            this.bookForm.patchValue(book);
+          }
+        },
+        error: err => {
+          this.error = 'Error fetching book';
+          console.error('Error fetching book:', err);
+        }
+      });
+    }
+  }
+  onSubmit(): void {
+    if (this.bookForm.valid) {
+      const book: Books = this.bookForm.value;
+      if (book.id) {
+        this.bookService.updateBook(book).subscribe(() => this.router.navigate(['/books']));
+      } else {
+        book.id = this.bookService.generateNewId();
+        this.bookService.addBook(book).subscribe(() => this.router.navigate(['/books']));
+      }
+    }
+  }
+  onDelete(): void {
+    const id = this.bookForm.value.id;
+    if (id) {
+      this.bookService.deleteBook(id).subscribe(() => this.router.navigate(['/books']));
+    }
+  }
+  navigateToBookList(): void {
+    this.router.navigate(['/books']);
+  }
 }
--- src/app/services/book.service.ts
+++ src/app/services/book.service.ts
@@ -31,6 +31,11 @@ private books : Books[] = booklist;
     this.books = this.books.filter(book => book.id !== bookId);
     return of(this.books);
   }
+
+  generateNewId(): number {
+    return this.books.length > 0 ? Math.max(...this.books.map(book => book.id)) + 1 : 1;
+  }
+
   getBookById(bookId: number): Observable<Books | undefined> {
     const book = this.books.find(book => book.id === bookId);
     return of(book);
