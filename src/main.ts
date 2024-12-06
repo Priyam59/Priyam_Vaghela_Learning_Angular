cd810ebcfb3ebe562dc44200dff5e1e82535f81a
Priyamsinh Vaghela <W0851705@myscc.ca>
2024-12-06 03:22:56 -0500

    I kept my default route to eager loaded and changed the remaining 3 of them to lazy load

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,26 @@ import {bootstrapApplication} from '@angular/platform-browser';
 import {AppComponent} from './app/app.component';
 import {provideRouter, Routes} from '@angular/router';
 import {BookListComponent} from "./app/book-list/book-list.component";
-import {BookListItemComponent} from "./app/book-list-item/book-list-item.component";
-import {PageNotFoundComponent} from "./app/page-not-found/page-not-found.component";
-import {ModifyBookComponent} from "./app/modify-book/modify-book.component";
+import {HttpClient, provideHttpClient} from "@angular/common/http";
+import {importProvidersFrom} from "@angular/core";
 
 const routes: Routes = [
   {path:'', redirectTo: '/books', pathMatch: 'full'},
   { path: 'books', component: BookListComponent },
-  { path: 'books/:id', component: BookListItemComponent },
-  {path:'modify-book  ', component: ModifyBookComponent},
-  {path: '**', component:PageNotFoundComponent}
+  { path: 'books/:id',
+    loadComponent: ()=>
+      import('./app/book-list-item/book-list-item.component').then(m=>m.BookListItemComponent) },
+  {path:'modify-book  ',
+    loadComponent: ()=>
+      import('./app/modify-book/modify-book.component').then(m=>m.ModifyBookComponent) },
+  {path: '**',
+    loadComponent:()=>
+      import('./app/page-not-found/page-not-found.component').then(m=>m.PageNotFoundComponent) },
 ];
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideHttpClient(),
+    provideRouter(routes),
+  // importProvidersFrom(HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService,{delay:1}))
+  ],
 }).catch((err) => console.error(err));
